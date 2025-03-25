f7c31f867f67002ce81e7b8d06431bf78b0e9cec
piyush rai <piyushrai7271@gmail.com>
2025-03-25 14:29:45 +0530

    other basic setup

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,6 +1,12 @@
-import {Router} from "express";
-import { logoutUser, registerUser } from "../controllers/user.controller.js";
-import {loginUser} from "../controllers/user.controller.js";
+import { Router } from "express";
+import {
+  changeCurrentPassword,
+  logoutUser,
+  registerUser,
+  loginUser,
+  getCurrentUser,
+  updateAccountDetails,
+} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
@@ -8,9 +14,11 @@ const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
-
 // secured routes.....
 
-router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/change-password").put(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-userDetails").put(verifyJWT, updateAccountDetails);
 
-export default router; 
+export default router;
